3991e377cc588cea49c3c5db14f371db6dd3e1dd
Abner Garbin <abner.batysta151@gmail.com>
2024-11-17 18:58:21 -0300

    Criando Serviço de autenticação

--- Services/CPFAutenticator.java
+++ Services/CPFAutenticator.java
@@ -1,6 +1,36 @@
 package Services;
 
+import entities.PersonalNumber;
+
+import java.util.concurrent.ExecutionException;
+
 public class CPFAutenticator {
+
+    private PersonalNumber personalNumber;
+
+    public CPFAutenticator(){
+    }
+
+    public CPFAutenticator(PersonalNumber personalNumber){
+        this.personalNumber = personalNumber;
+    }
+
+    public boolean verificaSeTodosOsNumerosSaoIguais(){
+        String number = personalNumber.getCpfNumber().toString();
+        for(int i = 0; i<number.length(); i++){
+
+        }
+    }
+
+    public boolean cpfValidate(){
+        String number = personalNumber.getCpfNumber().toString();
+
+        if (number.length() != 11){
+            throw new IllegalArgumentException("Número Inválido!") ;
+        } else if ( )
+
+        return false;
+    }
 
 
 }
