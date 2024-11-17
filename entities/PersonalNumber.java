35543cfe904305cbadec91ff874bfa3d31144020
Abner Garbin <abner.batysta151@gmail.com>
2024-11-17 18:12:37 -0300

    Criando a classe PersonalNumber

--- entities/PersonalNumber.java
+++ entities/PersonalNumber.java
@@ -1,4 +1,21 @@
 package entities;
 
 public class PersonalNumber {
+
+    private Double cpfNumber;
+
+    public PersonalNumber(){
+    }
+
+    public PersonalNumber(double cpfNumber){
+        this.cpfNumber = cpfNumber;
+    }
+
+    public Double getCpfNumber() {
+        return cpfNumber;
+    }
+
+    public void setCpfNumber(Double cpfNumber) {
+        this.cpfNumber = cpfNumber;
+    }
 }
